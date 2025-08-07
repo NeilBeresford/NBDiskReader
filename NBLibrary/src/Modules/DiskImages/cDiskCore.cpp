1b8109c18f74b2946d93604e7114a8781098b5f3
Neil Beresford <neil.beresford@gmail.com>
2025-08-07 11:39:11 +0100

    Small change, using terminal in nvim

--- NBLibrary/src/Modules/DiskImages/cDiskCore.cpp
+++ NBLibrary/src/Modules/DiskImages/cDiskCore.cpp
@@ -1,7 +1,7 @@
 /**
-@file       Test_File.cpp
-@brief      This file contains file handling tests for the project.
-@defgroup   ModernCppTests Modern C++ Tests
+@file       cDiskCore.cpp
+@brief      Core functionality needed for Disk Image Handling.
+@defgroup   NBLibraryFileHandling File Handling
 @copyright  Copyright (c) 2025, Neil Beresford
 */
 
@@ -9,3 +9,25 @@
 // Libraries
 // ----------------------------------------------------------------------------
 
+#include "cDiskCore.h"
+
+//-----------------------------------------------------------------------------
+// Class functionality
+// ----------------------------------------------------------------------------
+
+/**
+    @brief      Default constructor for DiskCore class.
+    @details    Initializes member variables to default values.
+    @ingroup    NBLibraryFileHandling
+*/
+cDiskCore::DiskCore()
+{
+    // initialize member variables
+    DiskTitle   = "NBDisk";
+    DiskName    = "NBDisk.d64";
+    OutDiskName = DiskName;
+}
+
+//-----------------------------------------------------------------------------
+// End of file: cDiskCore.cpp
+// ----------------------------------------------------------------------------
